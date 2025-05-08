d91609c1667f213a84a4527ba5c46aa29f7124c9
Kawser420 <muhammadkawser202@gmail.com>
2025-05-08 23:29:06 +0600

    porblem one solved

--- ts-Assignment/src/assignment.ts
+++ ts-Assignment/src/assignment.ts
@@ -1,2 +1,7 @@
 {
+  // problem solved---->01
+  function formatString(input: string, toUpper?: boolean): string {
+    const strToUpper = toUpper === undefined ? true : toUpper;
+    return strToUpper ? input.toUpperCase() : input.toLowerCase();
+  }
 }
