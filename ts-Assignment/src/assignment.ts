28dea5ca3ba3e12cf150157f922ef02813174eab
Kawser420 <muhammadkawser202@gmail.com>
2025-05-08 23:40:45 +0600

    problem two solved

--- ts-Assignment/src/assignment.ts
+++ ts-Assignment/src/assignment.ts
@@ -5,3 +5,24 @@
     return strToUpper ? input.toUpperCase() : input.toLowerCase();
   }
 }
+//
+{
+  // problem solved---->02
+  function filterByRating(
+    items: { title: string; rating: number }[]
+  ): { title: string; rating: number }[] {
+    return items.filter((item) => item.rating >= 4);
+  }
+  const books = [
+    { title: "Book A", rating: 4.44 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 },
+    { title: "Book d", rating: 3.999 },
+    { title: "Book e", rating: 10 },
+  ];
+  console.log(filterByRating(books));
+}
+//
+{
+  // problem solved---->01
+}
